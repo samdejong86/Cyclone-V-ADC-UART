b5a7e4a315b88b831c01db37da6501f602a6b9ba
srdejong <srdejong@uvic.ca>
2017-10-11 14:03:43 -0700

    comments added to getData.py

--- scripts/getData.py
+++ scripts/getData.py
@@ -3,19 +3,23 @@
 import serial
 import matplotlib.pyplot as plt
 
+#set the serial port settings
 set_ser = serial.Serial()
-set_ser.port="/dev/ttyUSB1"
-set_ser.baudrate=1000000
+set_ser.port="/dev/ttyUSB1" 
+set_ser.baudrate=1000000 
 set_ser.parity = serial.PARITY_NONE
 set_ser.stopbits=serial.STOPBITS_ONE
 set_ser.bytesize = serial.EIGHTBITS
 set_ser.timeout=1
 
-
+#open the serial port
 set_ser.open()
 
-message="ds"
+#send a message (can be anything)
+message="ds" 
 set_ser.write(message.encode('utf-8'))
+
+#recieve a response
 line=set_ser.read(5000)
 
 i=0
@@ -24,21 +28,27 @@ ADC=0
 xx=[]
 yy=[]
 
+#loop over response. response looks like:
+# xyz
+# where xy is the ADC counts, z is the time.
+
 for c in line:
-    #print(c)
-    if i%3==0:
-        lastC=c<<8
+    if i%3==0:        
+        lastC=c<<8  #bit shift 'x' by 8
     elif i%3==1:
-        ADC=lastC+c
+        ADC=lastC+c #add bit shifted x to y
     elif i%3==2:
-        xx.append((c-32)*25)
+        xx.append((c-32)*25) 
         yy.append(ADC)
     i=i+1
     
+#close serial port
 set_ser.close()
+
 
 plt.xlabel("Time (ns)")
 plt.ylabel("ADC counts (AU)")
 
+#plot ADC counts vs time
 plt.plot(xx, yy)
 plt.show()
