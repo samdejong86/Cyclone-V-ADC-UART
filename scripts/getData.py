bddc88a70b84a6dfa39f2163d90e3db6b4860baf
Sam de Jong <samdejong86@gmail.com>
2017-10-23 13:28:15 -0700

    minor change

--- scripts/getData.py
+++ scripts/getData.py
@@ -6,7 +6,7 @@ import numpy as np
 
 #set the serial port settings
 set_ser = serial.Serial()
-set_ser.port="/dev/ttyUSB1"          #the location of the USB port 
+set_ser.port="/dev/ttyUSB0"          #the location of the USB port 
 set_ser.baudrate=1000000             #baud rate of 1MHz
 set_ser.parity = serial.PARITY_NONE
 set_ser.stopbits=serial.STOPBITS_ONE
